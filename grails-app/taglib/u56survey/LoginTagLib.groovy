fb7f2631b6260f6c7f0f041fc72ae06e8f6667f9
kimsooil <kimsooil@gmail.com>
2011-05-05 17:21:36 +0000

    renamed 'logout' to 'switch user'

--- grails-app/taglib/u56survey/LoginTagLib.groovy
+++ grails-app/taglib/u56survey/LoginTagLib.groovy
@@ -4,7 +4,7 @@ class LoginTagLib {
 	def loginControl = {
 		if(session.user){
 		  out << message(code:"hello")+"  ${session.user} "
-		  out << """[${link(action:"logout", controller:"person"){"Logout"}}]"""
+		  out << """[${link(action:"logout", controller:"person"){"Switch user"}}]"""
 		  
 		  out << """-[${link(action:"securelogout", controller:"person"){"<label style='color:red'>SecureLogout</label>"}}]"""
 		  
