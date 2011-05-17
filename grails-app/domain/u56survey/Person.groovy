88ab159bbe6ac17333619613e3856cd3ae6d7cde
kimsooil <kimsooil@gmail.com>
2011-05-17 15:09:56 +0000

    Added more info about surveyers

--- grails-app/domain/u56survey/Person.groovy
+++ grails-app/domain/u56survey/Person.groovy
@@ -4,17 +4,57 @@ class Person {
   static constraints = {
     login(unique:true)
     password(password:true)
-	role(inList:["surveyer", "surveyee", "admin"])
+	role(inList:["surveyer", "admin"])
 	location(inList:["MOFF", "SANL"])
 	language(inList:["English", "Spanish"])
+	sex(inList:["Female", "Male"])
   }
   
-  String login
+  String login // userid
   String password
   String name
   String location // 4-letter code
   String language
   String role = "surveyer"
+  
+  // additional info
+  String locationName
+  String locAddress
+  String locCity
+  String locState
+  String locCountry
+  String locZipCode
+  String locAreaCode
+  String locPhoneNum
+  String locPhoneExt
+  String nameFirst
+  String nameMiddle
+  String nameLast
+  Date dob
+  String sex
+  String ssn
+  String email1
+  String email2
+  String phone1Area
+  String phone1Num
+  String phone1Ext
+  String phone2Area
+  String phone2Num
+  String phone2Ext
+  String homeAddress
+  String homeCity
+  String homeState
+  String homeCountry
+  String homeZipCode
+  String PrimaryPhoneArea
+  String PrimaryPhoneNum
+  String PrimaryPhoneExt
+  String EmergencyContact_FirstName
+  String EmergencyContact_MiddleName
+  String EmergencyContact_LastName
+  String EmergencyContact_email
+  String EmergencyContact_phone
+  String assignedToLocation
   
   String toString(){
     name
