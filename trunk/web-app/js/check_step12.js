cbe5ca6b9182f357a028c01d0e649db7faadb139
kimsooil@gmail.com <kimsooil@gmail.com@f18ad19d-50a7-e19c-d952-68a3e7bd993e>
2011-07-08 18:41:23 +0000

    Step9's dates(date/month/year) are validated

--- trunk/web-app/js/check_step12.js
+++ trunk/web-app/js/check_step12.js
@@ -93,7 +93,118 @@ function checkForm12()
 
 			if (fv.isEmpty($('#q84_another').val()) || fv.isEmpty($('#q84_10_whatAge').val()) || !fv.isValidNumber($('#q84_10_whatAge').val(), 1, ageCalculated))
 				fv.raiseError(i18nmessages.step12Err3 +"(#10)");
-		}	
+		}
+		if ( ($("#q84_1_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_1_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.1: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_1_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_1_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_1_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.1: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_2_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_2_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.2: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_2_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_2_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_2_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.2: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_3_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_3_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.3: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_3_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_3_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_3_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.3: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_4_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_4_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.4: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_4_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_4_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_4_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.4: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_5_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_5_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.5: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_5_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_5_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_5_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.5: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_6_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_6_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.6: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_6_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_6_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_6_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.6: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_7_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_7_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.7: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_7_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_7_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_7_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.7: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_8_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_8_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.8: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_8_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_8_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_8_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.8: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_9_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_9_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.9: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_9_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_9_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_9_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.9: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		if ( ($("#q84_10_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_10_rad_date_month").val()) > today_month) // java.util.Calendar.MONTH is 0~11
+			){
+				fv.raiseError("Q89.10: "+i18nmessages.step7ErrInvalidDate);
+			}
+		else if ( ($("#q84_10_rad_date_year").val() == today_year.toString())  &&
+				(parseInt($("#q84_10_rad_date_month").val()) == today_month) && // java.util.Calendar.MONTH is 0~11
+				(parseInt($("#q84_10_rad_date_day").val()) > today_date)
+		){
+			fv.raiseError("Q89.10: "+i18nmessages.step7ErrInvalidDate);			
+		} 
+		
 	}
 	// all done
 	// if errors, display, else proceed
